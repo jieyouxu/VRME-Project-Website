bc6c593e947420f960a52a48976caf0b84f474e7
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-01-19 18:40:12 +0000

    infra: add minimal setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+import "./App.css";
+
+const App = () => (
+	<div className="main-wrapper">
+		<h1 className="text-center">Hello World</h1>
+	</div>
+);
+
+export default hot(module)(App);
