adcb8674e01cd1029965132bf1b56ad376b05367
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-01-20 20:00:36 +0000

    refactor: extract components

---
+++ src/components/project-title-initials/ProjectTitleInitials.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProjectTitleInitials = () => (
+	<h1 className="font-weight-bold home-title-font text-center">
+		<span className="initials-text-bg-d0">
+			<span className="theme-c-l-red">V</span>
+			<span className="theme-c-l-orange">R</span>
+			<span className="theme-c-l-yellow">M</span>
+			<span className="theme-c-l-blue">E</span>
+		</span>
+	</h1>
+);
+
+export default ProjectTitleInitials;
--- src/pages/home/ProjectSummary.jsx
+++ src/pages/home/ProjectSummary.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import {
 	Container,
-	Jumbotron,
 	Row,
 	Col,
 	Card,
@@ -12,40 +11,14 @@ import {
 	ListGroupItem
 } from "reactstrap";
 
+import ProjectTitleJumbotron from "./ProjectTitleJumobotron";
 import TodoAlert from "../../components/todo-alert/TodoAlert";
 
 import "./ProjectSummary.css";
 
 const ProjectSummary = () => (
 	<Container fluid className="project-summary-container">
-		<Jumbotron
-			fluid
-			className="justify-content-center project-summary-jumbotron"
-		>
-			<h1 className="font-weight-bold home-title-font text-center">
-				<span className="initials-text-bg-d0">
-					<span className="theme-c-l-red">V</span>
-					<span className="theme-c-l-orange">R</span>
-					<span className="theme-c-l-yellow">M</span>
-					<span className="theme-c-l-blue">E</span>
-				</span>
-			</h1>
-			<p className="text-center text-wrap project-name-p theme-c-base-l0">
-				<span className="detailed-title-text-bg-d0">
-					<span className="theme-c-l-red">V</span>
-					<span>irtual</span>
-					<span>&nbsp;</span>
-					<span className="theme-c-l-orange">R</span>
-					<span>eality</span>
-					<span>&nbsp;</span>
-					<span className="theme-c-l-yellow">M</span>
-					<span>eeting</span>
-					<span>&nbsp;</span>
-					<span className="theme-c-l-blue">E</span>
-					<span>nvironment</span>
-				</span>
-			</p>
-		</Jumbotron>
+		<ProjectTitleJumbotron />
 		<Container fluid className="theme-bg-base-d0 p-3">
 			<div className="content-skip" />
 			<Container>
