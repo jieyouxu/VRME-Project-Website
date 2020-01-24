d0ad2c4275e59d9e811ec1c23b117132571ba78e
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-01-24 05:55:07 +0000

    feat: add systems architecture

--- src/pages/design/index.jsx
+++ src/pages/design/index.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import PageContentContainer from "../../components/page-content-container/PageContentContainer";
 import PageTitle from "../../components/page-title/PageTitle";
+import InnerContentContainer from "../../components/inner-content-container/InnerContentContainer";
+import SystemsArchitecture from "./parts/SystemsArchitecture";
 
 const Prototype = () => (
 	<PageContentContainer>
 		<PageTitle title="Design" />
+		<InnerContentContainer>
+			<SystemsArchitecture />
+		</InnerContentContainer>
 	</PageContentContainer>
 );
 
