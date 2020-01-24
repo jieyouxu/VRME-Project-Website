045ffd5faad1472f5f0bb0e080bbf09dc60ca8b4
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-01-24 02:21:48 +0000

    infra: fix router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import NavBar from "./components/nav-bar/NavBar";
 import Home from "./pages/home";
