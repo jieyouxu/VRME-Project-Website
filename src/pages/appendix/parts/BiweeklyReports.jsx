244559fb76039f311c9b102791e720419d3ad023
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-04-06 02:30:04 +0800

    docs: add biweekly reports

--- src/pages/appendix/parts/BiweeklyReports.jsx
+++ src/pages/appendix/parts/BiweeklyReports.jsx
@@ -7,6 +7,9 @@ import ContentCardHeader from "../../../components/content-card-header/ContentCa
 
 import Report20191122 from "../../../resources/biweekly-reports/2019-11-22.pdf";
 import Report20191206 from "../../../resources/biweekly-reports/2019-12-06.pdf";
+import Report20200124 from "../../../resources/biweekly-reports/2020-01-24.pdf";
+import Report20200207 from "../../../resources/biweekly-reports/2020-02-07.pdf";
+import Report20200228 from "../../../resources/biweekly-reports/2020-02-28.pdf";
 
 const BiweeklyReports = () => (
 	<ContentCard>
@@ -21,6 +24,21 @@ const BiweeklyReports = () => (
 				<ListGroupItem className="theme-bg-base-d1">
 					<a href={Report20191206} target="_blank" rel="noopener noreferrer">
 						2019-12-06
+					</a>
+				</ListGroupItem>
+				<ListGroupItem className="theme-bg-base-d1">
+					<a href={Report20200124} target="_blank" rel="noopener noreferrer">
+						2020-01-24
+					</a>
+				</ListGroupItem>
+				<ListGroupItem className="theme-bg-base-d1">
+					<a href={Report20200207} target="_blank" rel="noopener noreferrer">
+						2020-02-07
+					</a>
+				</ListGroupItem>
+				<ListGroupItem className="theme-bg-base-d1">
+					<a href={Report20200228} target="_blank" rel="noopener noreferrer">
+						2020-02-28
 					</a>
 				</ListGroupItem>
 			</ListGroup>
