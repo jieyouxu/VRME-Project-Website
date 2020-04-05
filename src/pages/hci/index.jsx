a4da24230428b7dd5a4fe322e52d3b2b25607387
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-04-06 03:21:29 +0800

    docs: break into two components

--- src/pages/hci/index.jsx
+++ src/pages/hci/index.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import PageContentContainer from "../../components/page-content-container/PageContentContainer";
 import PageTitle from "../../components/page-title/PageTitle";
 import InnerPageContentContainer from "../../components/inner-page-content-container/InnerPageContentContainer";
+
 import UISketches from "./parts/UISketches";
+import HCIExplanation from './parts/HCIExplanation';
 
 const HCI = () => (
 	<PageContentContainer>
 		<PageTitle title="Human-Computer Interaction (HCI)" />
 		<InnerPageContentContainer>
 			<UISketches />
+			<HCIExplanation />
 		</InnerPageContentContainer>
 	</PageContentContainer>
 );
